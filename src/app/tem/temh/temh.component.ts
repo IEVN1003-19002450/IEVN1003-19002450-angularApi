5374581ba18e516a9d18da2f8a8376faed122d36
IEVN1003-19002450 <beto.pandabox@gmail.com>
2024-10-26 10:13:30 -0600

    25/10/2024

---
+++ src/app/tem/temh/temh.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temh',
+  standalone: true,
+  imports: [],
+  templateUrl: './temh.component.html',
+  styles: ``
+})
+export class TemhComponent {
+
+}
